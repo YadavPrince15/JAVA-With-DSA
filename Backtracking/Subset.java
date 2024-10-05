f6f91be09f5c940e7acfa1f37e0f2e0b023ff864
Prince Kumar Yadav <princeaky2006@gmail.com>
2024-10-05 07:24:39 +0530

    Add files via upload

---
+++ Backtracking/Subset.java
@@ -0,0 +1,19 @@
+package JAVA_CODE.Backtracking;
+
+public class Subset {
+    public static void findSubset(String str , String ans, int i){
+        //base case
+        if(i == str.length()){
+            System.out.println(ans);
+            return;
+        }
+        //yes choice 
+        findSubset(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubset(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findSubset(str,"", 0);
+    }
+}
